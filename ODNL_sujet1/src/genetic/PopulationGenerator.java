7addf5a83a7e2df0ed6284bceb2ca4211179ae41
Rodolphe <guitteny.rodolphe@gmail.com>
2015-01-22 03:37:51 +0100

    Il ne peut pas y avoir deux fois le meme parent(meme si c'était deja improbable)

--- ODNL_sujet1/src/genetic/PopulationGenerator.java
+++ ODNL_sujet1/src/genetic/PopulationGenerator.java
@@ -38,12 +38,23 @@ public class PopulationGenerator
 	{
 		Random rn = new Random();
 		Population newgen = new Population(p.sqE, p.size());
+		
+		int indexp1;
+		int indexp2;
+		
 		Sequence parent1;
 		Sequence parent2;
 		for (int i = 0; i < p.size(); i += 2)
 		{
-			parent1 = selectGoodParent(p);
-			parent2 = p.get(rn.nextInt(p.size()));
+			indexp1 = selectGoodParentIndex(p);
+			parent1 = p.get(indexp1);
+			
+			indexp2 = rn.nextInt(p.size());
+			while (indexp1 == indexp2)
+			{
+				indexp2 = rn.nextInt(p.size());
+			}
+			parent2 = p.get(indexp2);
 			BuildChildren(newgen, parent1, parent2);
 		}
 		
@@ -121,17 +132,17 @@ public class PopulationGenerator
 		return p;
 	}
 	
-	private Sequence selectGoodParent(Population population)
+	private int selectGoodParentIndex(Population population)
 	{
 		double p = Math.random();
 		double cumulativeProbability = 0.0;
 		for (int i = 0; i < population.size(); i++) {
 		    cumulativeProbability += 2*(i+1)/(population.size() * (population.size()+1));
 		    if (p <= cumulativeProbability) {
-		        return population.get(i);
+		        return i;
 		    }
 		}
-		return population.get(population.size() - 1);
+		return population.size() - 1;
 	}
 	
 	
